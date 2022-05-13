937332c6e6db6e0c3cf3fb5081715bc795b56ff2
David Felix <davidffelix@hotmail.com>
2022-05-13 09:23:45 -0300

    Insert SizedBox with Divider

--- lib/main.dart
+++ lib/main.dart
@@ -16,6 +16,7 @@ class MeuApp extends StatelessWidget {
         backgroundColor: Color(0xff032459),
         body: SafeArea(
           child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
             children: [
               CircleAvatar(
                 backgroundImage: AssetImage('imagens/david.jpeg'),
@@ -30,49 +31,50 @@ class MeuApp extends StatelessWidget {
                   fontFamily: 'Pacifico',
                 ),
               ),
-              Text('Desenvolvedor Flutter',
+              Text(
+                'Desenvolvedor Flutter',
                 style: TextStyle(
                   color: Colors.white,
                 ),
               ),
+              SizedBox(
+                  height: 20,
+                  width: 250,
+                  child: Divider(
+                    color: Colors.white,
+                  )),
               Card(
-                color: Colors.white,
-                margin: EdgeInsets.symmetric(
-                    vertical: 10,
-                    horizontal: 25),
+                margin: EdgeInsets.symmetric(vertical: 10, horizontal: 25),
                 child: ListTile(
                   leading: Icon(
-                      Icons.phone,
+                    Icons.phone,
+                    color: Color(0xff032459),
+                    size: 25,
+                  ),
+                  title: Text(
+                    '+55 21 9999-9999',
+                    style: TextStyle(
                       color: Color(0xff032459),
-                      size: 25,
+                      fontSize: 20,
                     ),
-                    title: Text('+55 21 9999-9999',
-                      style: TextStyle(
-                        color: Color(0xff032459),
-                        fontSize: 20,
-                      ),
-                    ),
+                  ),
                 ),
               ),
-
-
               Card(
-                color: Colors. white,
-                margin: EdgeInsets.symmetric(
-                    vertical: 10,
-                    horizontal: 25),
+                margin: EdgeInsets.symmetric(vertical: 10, horizontal: 25),
                 child: ListTile(
-                    leading: Icon(
-                      Icons.email,
+                  leading: Icon(
+                    Icons.email,
+                    color: Color(0xff032459),
+                    size: 25,
+                  ),
+                  title: Text(
+                    'davidffelix@hotmail.com',
+                    style: TextStyle(
                       color: Color(0xff032459),
-                      size: 25,
+                      fontSize: 20,
                     ),
-                    title: Text('davidffelix@hotmail.com',
-                      style: TextStyle(
-                        color: Color(0xff032459),
-                        fontSize: 20,
-                      ),
-                    ),
+                  ),
                 ),
               ),
             ],
