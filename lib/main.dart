e665ba0a9fc8dd1c36acacdf6adc162fa43697ef
David Felix <davidffelix@hotmail.com>
2022-06-08 14:31:38 -0300

    Remove debug banner

--- lib/main.dart
+++ lib/main.dart
@@ -12,6 +12,7 @@ class MeuApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         backgroundColor: Color(0xff032459),
         body: SafeArea(
